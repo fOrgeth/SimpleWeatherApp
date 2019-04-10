cca5ba83478ffa420b7ee8c6819cf19559063046
fOrgeth <ivdovin@mail.ru>
2019-04-10 11:03:58 +0300

    Revert " Your branch is up to date with 'origin/feature_cities_list'."
    This reverts commit edb6be48a249abaad9e8065b145d93a28487cdf8.

--- app/src/main/java/th/forge/simpleweatherapp/presentation/MainActivity.java
+++ app/src/main/java/th/forge/simpleweatherapp/presentation/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, (DrawerLayout) null);
     }
 
-    //ToDo: release navigation
     public void show(Location location) {
         CurrentWeatherFragment currentWeatherFragment = CurrentWeatherFragment.newInstance(location.getName());
         getSupportFragmentManager().beginTransaction()
